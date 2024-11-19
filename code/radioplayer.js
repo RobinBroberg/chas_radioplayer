dec6e0180a39490de7141e5e4c7b6daf97b59aaf
RobinBroberg <robin.broberg86@gmail.com>
2024-11-19 12:08:37 +0100

    last commit

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -1,8 +1,5 @@
 // Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
 const channelsElement = document.getElementById("channels");
-channelsElement.style.display = "flex";
-channelsElement.style.flexDirection = "column";
-channelsElement.style.alignItems = "center";
 
 async function getRadio() {
   const response = await fetch(
@@ -12,23 +9,15 @@ async function getRadio() {
 
   data.channels.forEach((channel) => {
     const channelDiv = document.createElement("div");
-    channelDiv.style.display = "flex";
-    channelDiv.style.alignItems = "center";
+    channelDiv.setAttribute("class", "channelDiv");
     channelDiv.style.backgroundColor = "#" + channel.color;
-    channelDiv.style.padding = "10px";
-    channelDiv.style.margin = "10px";
-    channelDiv.style.borderRadius = "10px";
-    channelDiv.style.width = "800px";
-    channelDiv.style.maxWidth = "90%";
 
     const imageDiv = document.createElement("div");
     const image = document.createElement("img");
+    image.setAttribute("class", "image");
     image.src = channel.image;
     image.alt = channel.imagetemplate;
-    image.style.width = "200px";
-    image.style.marginRight = "20px";
-    image.style.maxWidth = "100%";
-    imageDiv.appendChild(image);
+    imageDiv.append(image);
 
     const contentDiv = document.createElement("div");
     contentDiv.style.display = "flex";
@@ -39,22 +28,17 @@ async function getRadio() {
     channelName.style.marginLeft = "10px";
 
     const audioTag = document.createElement("audio");
-    audioTag.setAttribute("controls", "controls");
-    audioTag.style.maxWidth = "100%";
+    audioTag.controls = true;
+    audioTag.setAttribute("class", "audio");
 
     const audioSource = document.createElement("source");
     audioSource.src = channel.liveaudio.url;
     audioSource.type = "audio/mpeg";
 
-    audioTag.appendChild(audioSource);
-
-    contentDiv.appendChild(channelName);
-    contentDiv.appendChild(audioTag);
-
-    channelDiv.appendChild(imageDiv);
-    channelDiv.appendChild(contentDiv);
-
-    channelsElement.appendChild(channelDiv);
+    audioTag.append(audioSource);
+    contentDiv.append(channelName, audioTag);
+    channelDiv.append(imageDiv, contentDiv);
+    channelsElement.append(channelDiv);
   });
 }
 
